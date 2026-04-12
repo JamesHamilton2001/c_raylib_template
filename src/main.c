bb7f7af58e9112e7bdc98240ce8dc399a4bfd902
james <james@james.james>
2026-04-12 22:09:42 +0000

    fade with render texture implementation

--- src/main.c
+++ src/main.c
@@ -15,7 +15,11 @@ int screenWidth;
 int screenHeight;
 int targetFps;
 
+float fade;
+
 CellularAutomaton cellularAutomaton;
+
+RenderTexture2D targetRenderTexure;
 
 
 
@@ -52,12 +56,14 @@ static void init( void )
     screenHeight = 1024;
     targetFps = 240;
 
+    fade = 0.1f;
+
     InitWindow( screenWidth, screenHeight, "c_raylib_template" );
     SetTargetFPS( targetFps );
 
     Color * initialStateColours = NULL;
-    int32_t rows = 1024;
-    int32_t cols = 1024;
+    int32_t rows = 512;
+    int32_t cols = 512;
     uint32_t id = 0;
     uint32_t seed = 0x42424242;
     CellState * initialStates = NULL;
@@ -71,12 +77,16 @@ static void init( void )
         seed,
         initialStates
     );
+
+    targetRenderTexure = LoadRenderTexture( screenWidth, screenHeight );
 }
 
 
 
 static void denit( void )
 {
+    UnloadRenderTexture( targetRenderTexure );
+    briansBrainDenit( &cellularAutomaton );
     CloseWindow( );
 }
 
@@ -91,11 +101,45 @@ static void update( void )
 
 static void draw( void )
 {
+    BeginTextureMode( targetRenderTexure );
+
+    DrawRectangle( 0, 0, targetRenderTexure.texture.width, targetRenderTexure.texture.height, Fade( BLACK, fade ) );
+
+    CellularAutomatonDraw( &cellularAutomaton );
+
+    EndTextureMode( );
+
+
     BeginDrawing( );
 
     ClearBackground( BLACK );
 
-    CellularAutomatonDraw( &cellularAutomaton );
+    Rectangle src = {
+        0.0f,
+        0.0f,
+        (float)targetRenderTexure.texture.width,
+        -(float)targetRenderTexure.texture.height,
+    };
+
+    Rectangle dst = {
+        0.0f,
+        0.0f,
+        (float)targetRenderTexure.texture.width,
+        (float)targetRenderTexure.texture.height,
+    };
+
+    Vector2 origin = { 0.0f, 0.0f };
+
+    float rotation = 0.0f;
+
+    DrawTexturePro(
+        targetRenderTexure.texture,
+        src,
+        dst,
+        origin,
+        rotation,
+        WHITE
+    );
 
     DrawFPS( 0, 0 );
 
